72d513132e8970937d384b8cc0ebd77bc4d98cd2
NancyGong03 <nancy.gong@mail.mcgill.ca>
2025-01-26 00:56:13 -0500

    anchored down the header and bottom banner

--- frontend/src/pages/bodyPartPage.jsx
+++ frontend/src/pages/bodyPartPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 // import back_arrow from "../assets/back_button.png";
-import question_mark from "../assets/question_mark.png";
+import question_mark from "../assets/question_mark_2.png";
 import bandaid from "../assets/bandaid_inverted.svg";
 
 // Import body parts images
@@ -38,7 +38,8 @@ const BodyPartPage = ({ bodyPart }) => {
 
     return (
         <div>
-            <header className="flex justify-between w-full px-4 py-2">
+            {/* For the archER title */}
+            <header className="absolute top-20 left-0 justify-between w-full px-1 py-2">
                 {/* <Link to="..">
                     <button>
                         <img
@@ -48,11 +49,15 @@ const BodyPartPage = ({ bodyPart }) => {
                         />
                     </button>
                 </Link> */}
-                <h1 className="text-5xl font-bold">
+                <h1 className="text-10xl">
                     <span className="text-black">arch</span>
-                    <span className="text-pink-600">ER</span>
+                    <span className="text-pink-600 font-bold">ER</span>
                 </h1>
-                <button>
+            </header>
+
+            {/* For the question mark button */}
+            <header className="absolute top-4 right-2 justify-between w-full px-1 py-2 bg-white">
+                <button className="absolute right-0 outline-none border-none shadow-none">
                     <img
                         src={question_mark}
                         alt="Question mark"
@@ -62,16 +67,16 @@ const BodyPartPage = ({ bodyPart }) => {
             </header>
 
             {/* Render body part image dynamically */}
-            <main className="flex-grow flex items-center justify-center">
+            <main className="flex flex-grow items-center justify-center w-screen h-full">
                 <img
                     src={image}
                     alt={bodyPart}
-                    className="w-3/4 h-3/4 object-cover rounded-2xl shadow-md"
+                    className="w-full h-full object-cover rounded-2xl shadow-none"
                 />
             </main>
 
             {/* Footer */}
-            <footer className="w-full bg-gray-100 p-4 rounded-t-2xl shadow-md">
+            <footer className="absolute bottom-0 left-0 w-full bg-gray-100 p-4 rounded-t-2xl shadow-md">
                 <div className="flex justify-between items-center">
                     {/* Footer buttons */}
 
